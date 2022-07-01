f8e529ca58ef2908e19b9335c8eadcb8ffd0e9f4
yuzuki-chi <47371712+yuzuki-chi@users.noreply.github.com>
2022-07-01 16:02:53 +0900

    cronの追加

---
+++ cron.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+
+cd `dirname $0`
+CWD=$(pwd)
+
+DATE=`TZ='Asia/Tokyo' date`
+echo $DATE >> "$CWD/git-cron.log"
+
+git fetch
+
+# Remote branch log
+GITREMOTE=$(git log main origin/main --pretty=format:"%H" | head -n 1)
+
+# Local branch log
+GITLOCAL=$(git log -n 1 --pretty=format:"%H")
+
+if [ "$GITREMOTE" = "$GITLOCAL" ] ; then
+    echo 'no changed' >> "$CWD/git-cron.log"
+else
+    echo 'Remote branch is changed' >> "$CWD/git-cron.log"
+    git pull origin main
+
+    curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' $(echo webhookurl)
+fi
